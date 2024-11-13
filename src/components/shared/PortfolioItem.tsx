e4747b7fa7adc024731355626ac838fbb0c69a12
MikkisGuy <82242822+mikkisguy@users.noreply.github.com>
2024-11-13 21:18:14 +0200

    Moved file outside of components folder

--- src/components/shared/PortfolioItem.tsx
+++ src/components/shared/PortfolioItem.tsx
@@ -1,6 +1,6 @@
 import { useTranslation } from "react-i18next";
 import styled from "styled-components";
-import PORTFOLIO_IMAGES from "./portfolioImages";
+import PORTFOLIO_IMAGES from "../../shared/portfolioImages";
 
 type PortfolioItemProps = {
   isHobbyProject?: boolean;
