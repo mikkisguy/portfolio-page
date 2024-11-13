a8dbcd4558cfa7895139073d379bbc8c3c1853d5
MikkisGuy <82242822+mikkisguy@users.noreply.github.com>
2024-11-13 21:39:01 +0200

    Added lahti-huvilat images

--- src/shared/portfolioImages.ts
+++ src/shared/portfolioImages.ts
@@ -1,15 +1,21 @@
-import netvisor1Thumb from "../../assets/images/items/netvisor-1-thumb.webp";
-import netvisor2Thumb from "../../assets/images/items/netvisor-2-thumb.webp";
-import netvisor3Thumb from "../../assets/images/items/netvisor-3-thumb.webp";
-import netvisor1 from "../../assets/images/items/netvisor-1.webp";
-import netvisor2 from "../../assets/images/items/netvisor-2.webp";
-import netvisor3 from "../../assets/images/items/netvisor-3.webp";
-import peliruukku1Thumb from "../../assets/images/items/peliruukku-1-thumb.webp";
-import peliruukku2Thumb from "../../assets/images/items/peliruukku-2-thumb.webp";
-import peliruukku3Thumb from "../../assets/images/items/peliruukku-3-thumb.webp";
-import peliruukku1 from "../../assets/images/items/peliruukku-1.webp";
-import peliruukku2 from "../../assets/images/items/peliruukku-2.webp";
-import peliruukku3 from "../../assets/images/items/peliruukku-3.webp";
+import netvisor1Thumb from "../assets/images/items/netvisor-1-thumb.webp";
+import netvisor2Thumb from "../assets/images/items/netvisor-2-thumb.webp";
+import netvisor3Thumb from "../assets/images/items/netvisor-3-thumb.webp";
+import netvisor1 from "../assets/images/items/netvisor-1.webp";
+import netvisor2 from "../assets/images/items/netvisor-2.webp";
+import netvisor3 from "../assets/images/items/netvisor-3.webp";
+import lahtihuvilat1Thumb from "../assets/images/items/lahtihuvilat-hallitilaafi-1-thumb.webp";
+import lahtihuvilat2Thumb from "../assets/images/items/lahtihuvilat-hallitilaafi-2-thumb.webp";
+import lahtihuvilat3Thumb from "../assets/images/items/lahtihuvilat-hallitilaafi-3-thumb.webp";
+import lahtihuvilat1 from "../assets/images/items/lahtihuvilat-hallitilaafi-1.webp";
+import lahtihuvilat2 from "../assets/images/items/lahtihuvilat-hallitilaafi-2.webp";
+import lahtihuvilat3 from "../assets/images/items/lahtihuvilat-hallitilaafi-3.webp";
+import peliruukku1Thumb from "../assets/images/items/peliruukku-1-thumb.webp";
+import peliruukku2Thumb from "../assets/images/items/peliruukku-2-thumb.webp";
+import peliruukku3Thumb from "../assets/images/items/peliruukku-3-thumb.webp";
+import peliruukku1 from "../assets/images/items/peliruukku-1.webp";
+import peliruukku2 from "../assets/images/items/peliruukku-2.webp";
+import peliruukku3 from "../assets/images/items/peliruukku-3.webp";
 
 type Images = {
   [key: string]: string;
@@ -22,6 +28,12 @@ const portfolioImages: Images = {
   "netvisor-1": netvisor1,
   "netvisor-2": netvisor2,
   "netvisor-3": netvisor3,
+  "lahtihuvilat-hallitilaafi-1-thumb": lahtihuvilat1Thumb,
+  "lahtihuvilat-hallitilaafi-2-thumb": lahtihuvilat2Thumb,
+  "lahtihuvilat-hallitilaafi-3-thumb": lahtihuvilat3Thumb,
+  "lahtihuvilat-hallitilaafi-1": lahtihuvilat1,
+  "lahtihuvilat-hallitilaafi-2": lahtihuvilat2,
+  "lahtihuvilat-hallitilaafi-3": lahtihuvilat3,
   "peliruukku-1-thumb": peliruukku1Thumb,
   "peliruukku-2-thumb": peliruukku2Thumb,
   "peliruukku-3-thumb": peliruukku3Thumb,
